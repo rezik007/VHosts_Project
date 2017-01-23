2dee3d4601365e7a111c6401928caa3df82a5b9e
rezik007 <rezik007@gmail.com>
2017-01-23 17:24:13 +0100

    newVersion

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from PyQt5.QtCore import pyqtSlot, Qt
 import sys, json, requests
 import gui
 import numpy as np
+import sys
+import csv
 
 class Application(QtWidgets.QMainWindow, gui.Ui_MainWindow):
 
@@ -18,15 +20,16 @@ class Application(QtWidgets.QMainWindow, gui.Ui_MainWindow):
 
     @pyqtSlot()
     def on_click1(self):
-        self.FindHosts()
+        self.AnalyzeTopDomains()
 
     def FindHosts(self):
 
         domainName = self.textEdit.toPlainText()
         requestOne = requests.get("http://reverseip.logontube.com/?url="+domainName+"&output=json")
         requestOne.raise_for_status()
-        # works for Python2 and Python3
         jsonRead = json.loads(requestOne.content.decode('utf-8'))
+        # works for Python2 and Python3
+
         requestTwo = requests.get("http://api.hackertarget.com/reverseiplookup/?q="+domainName)
         requestTwo.raise_for_status()
 
@@ -38,8 +41,27 @@ class Application(QtWidgets.QMainWindow, gui.Ui_MainWindow):
             self.textBrowser.append(jsonRead['response']['domains'][i])
         self.textBrowser.append(requestTwo.text)
 
+    def AnalyzeTopDomains(self):
+        summaryResult = {}
+
         databaseOfDomains = np.genfromtxt(r'top500domains.csv', dtype='U',skip_header=1, delimiter=',', usecols=(1,))
-        print(databaseOfDomains)
+        databaseOfRootDomains = np.genfromtxt(r'top500domains.csv', dtype='U',skip_header=1, delimiter=',', usecols=(2,))
+
+        for i in range(0, 20):
+            requestOneAuto = requests.get("http://reverseip.logontube.com/?url="+databaseOfDomains[i]+"&output=json")
+            requestOneAuto.raise_for_status()
+            jsonRead = json.loads(requestOneAuto.content.decode('utf-8'))
+            numberOfDomainsOne = len(jsonRead['response']['domains'])
+            requestTwoAuto = requests.get("http://api.hackertarget.com/reverseiplookup/?q=" +databaseOfDomains[i])
+            requestTwoAuto.raise_for_status()
+            numberOfDomainsTwo = len(requestTwoAuto.text.split('\n'))
+            summaryResult[databaseOfDomains[i]] = numberOfDomainsOne+numberOfDomainsTwo, databaseOfRootDomains[i]
+
+        print(summaryResult)
+        with open('new.csv', 'w', newline='') as csv_file:
+            writer = csv.writer(csv_file )
+            for key, value in summaryResult.items():
+                writer.writerow([key, value])
 
 
 if __name__ == '__main__':
